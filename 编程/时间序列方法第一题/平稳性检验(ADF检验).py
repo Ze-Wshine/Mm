d9f7bd6e8f4251f61089567daf9214df1e23dfdb
Liangyu Qu <q2144544081@gmail.com>
2025-05-01 00:32:16 +0800

    Create 平稳性检验(ADF检验).py

---
+++ 编程/时间序列方法第一题/平稳性检验(ADF检验).py
@@ -0,0 +1,30 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from statsmodels.tsa.stattools import adfuller
+plt.rcParams['font.sans-serif'] = ['Microsoft YaHei']
+
+# 读取Excel数据
+df = pd.read_excel(r'F:\数模校赛\Mm\编程\数据集\在校生人数历史数据.xls',sheet_name='17-10',engine='xlrd')
+
+# 执行 ADF 检验
+def adf_test(series, label):
+    result = adfuller(series)
+    print(f"\n【{label}】ADF检验统计量: {result[0]}")
+    print(f"{label}的p值: {result[1]}")
+    if result[1] < 0.05:
+        print(f"→ {label} 是平稳的")
+    else:
+        print(f"→ {label} 是非平稳的")
+
+adf_test(df['义务教育在校生'], '义务教育')
+adf_test(df['高中教育在校生'], '高中教育')
+adf_test(df['高等教育在校生'], '高等教育')
+
+#最终结果为义务教育和高等教育为非平稳，故对义务教育和高等教育在校生进行一阶差分。
+df['义务教育差分'] = df['义务教育在校生'].diff()
+adf_test(df['义务教育差分'].dropna(), '义务教育一阶差分')
+
+df['高等教育差分'] = df['高等教育在校生'].diff()
+adf_test(df['高等教育差分'].dropna(), '高等教育一阶差分')
+#一阶差分后高等教育仍然非平稳，故不适合用时间序列方法做本题。
+
